8085469933393bf81141462471dcfc02e2419473
Cybernetics <mail@cybernetics.me>
2026-02-23 09:26:36 +0530

    small updates

--- app/Http/Controllers/WebController.php
+++ app/Http/Controllers/WebController.php
@@ -341,7 +341,7 @@ class WebController extends Controller
             "phone" => $request->contact_phone ?? "NA",
             "message" => $request->message ?? "NA"
         ];
-        //Mail::to($this->admin_email)->send(new FormSubmitNotificationEmail($data));
+        Mail::to($this->admin_email)->send(new FormSubmitNotificationEmail($data));
         return redirect()->route('message');
     }
 
